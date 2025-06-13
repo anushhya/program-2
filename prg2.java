9e8538745ce86c4a2b3d64a0f87c32eaa8fec1d3
anushhya <24203004@srcas.ac.in>
2025-06-13 12:22:16 +0530

    Create prg2.java

---
+++ prg2.java
@@ -0,0 +1,6 @@
+public class prg2{
+  public static void main(string[] args){
+  system.out.println("welcome");
+  system.out.println("to");
+  system.out.println("Devops");
+   system.out.println("class");
