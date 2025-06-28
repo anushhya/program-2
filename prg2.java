9a981276b5be96ef2ddb9a27e6e34e4100c76a7e
anushhya <24203004@srcas.ac.in>
2025-06-28 11:22:59 +0530

    Update prg2.java

--- prg2.java
+++ prg2.java
@@ -1,5 +1,5 @@
 public class prg2{
-  public static void main(String[] args){
+  public static void main(String[]args){
   system.out.println("welcome");
   system.out.println("to");
   system.out.println("Devops");
